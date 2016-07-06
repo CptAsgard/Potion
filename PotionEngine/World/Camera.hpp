a960c7f241ca48b30c292d35da7c61d4c45f143a
Frank van Wattingen <f.vanwattingen@gmail.com>
2016-07-06 10:25:53 +0200

    Moved Camera from perforce to github

--- PotionEngine/World/Camera.hpp
+++ PotionEngine/World/Camera.hpp
@@ -5,24 +5,51 @@
 #include "..\Math\Matrix.hpp"
 
 #include "..\World\GameObject.hpp"
+#include "..\Systems\UniqueID.hpp"
+
+#include "..\Rendering\TextureCube.hpp"
+#include "..\Rendering\TextureRender.hpp"
 
 namespace Potion
 {
+	// TODO: Camera culling mask
 	class Camera : public GameObject
 	{
 	public:
 		Camera( float fovy, float aspectRatio, float zNear, float zFar );
+		virtual ~Camera();
 
-		~Camera();
+		void StartDraw();
 
-		void Render( GameObject & go );
+		void EndDraw();
 
-		// Get MVP from your Model matrix
-		const Matrix Camera::TransformModelMatrixToMVP( const Matrix& model );
+		uint8_t GetCameraID() const;
 
-	private:
-		Matrix m_projectionMatrix;
-		Matrix m_viewMatrix;
+		bool IsCulled( uint32_t layer ) const;
+
+		void SetSkybox( TextureCube* cubemap );
+
+		void SetRenderTarget( TextureRender* textureRender );
+
+		Matrix* GetProjectionMatrix();
+		Matrix* GetViewMatrix();
+
+	protected:
+		virtual void Initialize( SceneManager * game ) = 0;
+
+		virtual void Update( SceneManager * game, float deltaTime ) = 0;
+
+		Model skyBox;
+		TextureCube* skyboxTex;
+
+		Model renderTarget;
+		TextureRender* renderTargetTex;
+
+		static UniqueID uniqueID;
+		uint8_t cameraID;
+
+		Matrix projectionMatrix;
+		Matrix viewMatrix;
 
 	};
 
